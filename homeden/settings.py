1609f29a9cefe0dd1639f437593a16aa3f4e717c
Vivek248-cloud <vivekofficial248.com>
2026-04-17 11:12:06 +0530

    new update

--- homeden/settings.py
+++ homeden/settings.py
@@ -80,16 +80,16 @@ WSGI_APPLICATION = 'homeden.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'homedeninvoice',
-        'USER': 'root',              
-        'PASSWORD': 'Admin123',  
-        'HOST': '127.0.0.1',         
-        'PORT': '3306',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'homedeninvoice',
+#         'USER': 'root',              
+#         'PASSWORD': 'Admin123',  
+#         'HOST': '127.0.0.1',         
+#         'PORT': '3306',
+#     }
+# }
 
 
 
